e24b1ab944054342799eb55d59049643edbaf604
ecappz3420 <info@ecappz.in>
2025-06-27 20:32:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import { getRecords } from "./api/zoho.js";
+import dayjs from "dayjs";
+import { databases } from "./api/appwriteClient.js";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(
+  cors({
+    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const pushToAppWrite = async () => {
+  try {
+    const today = dayjs(new Date()).format("DD-MMM-YYYY");
+    const response = await getRecords(
+      "All_Maintenance_Scheduler_Task_List",
+      `(Schedule_Date == '${today}')`
+    );
+    if (response.code !== 3000) {
+      console.log("Zoho API Error", response);
+      return;
+    }
+
+    for (const record of response.data) {
+      const payload = {
+        task: record.Task_Name,
+        field_type: record.Field_Type.zc_display_value,
+        remarks: record.Remarks,
+        site_id: parseInt(record.Site),
+        flag: record.Flags_For_Review == true ? true : false,
+        image_mandatory: record.Image_Mandatory == true ? true : false,
+        maintenance_scheduler_id: parseInt(record.Maintenance_Scheduler_ID?.ID),
+        maintenance_master_id: parseInt(record.Maintenance_Master),
+        schedule_date: record.Schedule_Date,
+        area: record.Area,
+        status: record.Status,
+      };
+      try {
+        const doc = await databases.createDocument(
+          "sjpl_zoho",
+          "685e6053002f1a36dc63",
+          record.ID.toString(),
+          payload
+        );
+        console.log("✅ Created:", doc.$id);
+      } catch (error) {
+        console.log("❌ Error creating document", error.message);
+      }
+      await sleep(200);
+    }
+  } catch (error) {
+    console.error("❌ Error in pushToAppWrite", error.message);
+  }
+};
+pushToAppWrite();
+
+const PORT = process.env.PORT || 4004;
+
+app.get("/", async (req, res) => {
+  return res
+    .status(200)
+    .json({ message: "Smart Joules Zoho and Appwrite function" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
